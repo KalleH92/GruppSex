147f34862566dd5b6134ad8e88f61acafa69fef2
KalleH92 <kalle.hagberg92@hotmail.com>
2024-06-10 13:00:48 +0200

    first commit

---
+++ src/main/java/org/example/gruppsex/GruppSexApplication.java
@@ -0,0 +1,13 @@
+package org.example.gruppsex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GruppSexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GruppSexApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/gruppsex/GruppSexApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.gruppsex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GruppSexApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
