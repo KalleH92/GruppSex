bb9f7bd81d57ae529c1c978b53cede02974d2fac
KalleH92 <kalle.hagber92@hotmail.com>
2024-06-11 09:34:50 +0200

    first h2

---
+++ src/main/java/org/example/gruppsex/config/SecurityConfig.java
@@ -0,0 +1,39 @@
+package org.example.gruppsex.config;
+
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+
+public class SecurityConfig {
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers("/h2-console/**");
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.ignoringRequestMatchers("/register"))
+                .authorizeHttpRequests(
+                        authorizeRequest -> authorizeRequest.requestMatchers("/admin")
+                                .hasRole("ADMIN").requestMatchers("/")
+                                .permitAll()
+                                .requestMatchers("/register")
+                                .permitAll()
+                                .anyRequest()
+                                .authenticated());
+
+
+        return http.build();
+
+    }
+
+}
