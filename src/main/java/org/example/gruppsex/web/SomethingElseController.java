c39d3a6575044d1b250e5ab13b5add83a9feee08
KalleH92 <kalle.hagberg92@hotmail.com>
2024-06-15 21:26:41 +0200

    loggning

--- src/main/java/org/example/gruppsex/web/SomethingElseController.java
+++ src/main/java/org/example/gruppsex/web/SomethingElseController.java
@@ -10,6 +10,8 @@ import org.example.gruppsex.model.UserDTO;
 import org.example.gruppsex.repository.UserRepository;
 import org.example.gruppsex.service.Maskning;
 import org.example.gruppsex.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -28,6 +30,7 @@ public class SomethingElseController {
     private final UserRepository userRepository;
     private final PasswordEncoder encoder;
     private final UserService userService;
+    private static final Logger logger = LoggerFactory.getLogger(SomethingElseController.class);
 
 
     public SomethingElseController(UserRepository userRepository, PasswordEncoder encoder, UserService userService) {
@@ -38,6 +41,7 @@ public class SomethingElseController {
 
     @GetMapping("/register")
     public String registreringsSida (Model model) {
+        logger.debug("Registrerings-sida.");
 
         model.addAttribute("user", new UserDTO());
 
@@ -46,10 +50,13 @@ public class SomethingElseController {
 
     @PostMapping("/register")
     public String submitForm (@Valid @ModelAttribute("user") UserDTO userDTO, BindingResult bindingResult, Model model) {
+        logger.debug("Registrerad användares information.");
 
         if (bindingResult.hasErrors()) {
+            logger.debug("Fel information angiven baserad på DTOobjektets krav.");
             return "registrera";
         } else {
+            logger.debug("Korrekt information baserad på DTOobjektets krav och lagt till användare " + Maskning.maskEmail(userDTO.getUsername()) + ".");
 
             //MyUser user = new MyUser();
 
@@ -73,6 +80,7 @@ public class SomethingElseController {
 
     @GetMapping("/list")
     public String userList (Model model) {
+        logger.debug("Registrerade användare.");
 
         model.addAttribute("users", userService.getAllUsers());
         return "userList";
@@ -80,10 +88,12 @@ public class SomethingElseController {
 
     @GetMapping("/list/{id}")
     public String singleUser (@PathVariable Long id, Model model) {
+        logger.debug("Information om användare med id: " + id + ".");
 
         boolean user = userService.getUserById(id).isPresent();
 
         if (user != false) {
+            logger.debug("Användare med id " + id + " kunde hittas.");
 
             MyUser user1 = userService.getUserById(id).get();
 
@@ -92,6 +102,7 @@ public class SomethingElseController {
             return "user";
 
         } else {
+            logger.warn("Användare med id " + id + " kunde ej hittas.");
 
             model.addAttribute("id", id);
             return "userNotFound";
@@ -101,10 +112,13 @@ public class SomethingElseController {
 
     @PostMapping("/list/{id}")
     public String updateUser ( @PathVariable Long id, @Valid @ModelAttribute("user") /*UserDTO*/UpdateUserDTO user, BindingResult result, Model model, MyUser myUser) {
+        logger.debug("Uppdatering av användares lösenord.");
 
         if (result.hasErrors()) {
+            logger.debug("Lösenord ej uppdaterat då det ej uppfyller kraven.");
             return "user";
         } else {
+            logger.debug("Lösenordet kan uppdateras.");
 
 //        user.setAge(user.getAge());
 //        user.setRole(user.getRole());
@@ -117,6 +131,8 @@ public class SomethingElseController {
         try {
             userService.updateUser(id, user);
         } catch (UsernameNotFoundException e) {
+            logger.warn("Användaren " + Maskning.maskEmail(user.getUsername()) + " hittades inte vid uppdatering.");
+            logger.error("UserNameNotFoundException: ", e);
             //implement logger.warn
             //stack trace the exception e
             System.out.println("username not found when updating user");
@@ -156,44 +172,53 @@ public class SomethingElseController {
 
     @GetMapping("/login")
     public String login () {
+        logger.debug("Användarnamn och lösenord ska anges.");
 
         return "login";
     }
 
     @GetMapping("/"/*"/loginsuccess"*/)
     public String loggedInSuccessfully(Model model) {
+        logger.debug("Inloggning har försökts.");
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
         Object principal = authentication.getPrincipal();
 
         if (principal instanceof org.springframework.security.core.userdetails.User) {
+            logger.debug("Principal är en User.");
+
             User user = (User) principal;
             MyUser user1 = userService.getUserByUsername(user.getUsername()).orElse(null);
 
             if (user1 != null) {
+                logger.debug("Inloggning för användare " + Maskning.maskEmail(user1.getUsername()) + "lyckats.");
                 model.addAttribute("user", user1);
                 return "loginSuccess";
             }
         } else if (principal instanceof String) {
+            logger.debug("Principal är en String.");
             String username = (String) principal;
             MyUser user1 = userService.getUserByUsername(username).orElse(null);
 
             if (user1 != null) {
+                logger.debug("Inloggning för användare " + Maskning.maskEmail(user1.getUsername()) + "lyckats.");
                 model.addAttribute("user", user1);
                 return "loginSuccess";
             }
         }
-
+        logger.warn("Username och/eller Password finns ej.");
         return "login";
     }
 
     @GetMapping("/error")
     public String showError () {
+        logger.debug("Error-sida.");
         return "error";
     }
 
     @GetMapping("/deleteuser")
     public String deleteUserForm (Model model) {
+        logger.debug("Borttagning av användare sida.");
 
         model.addAttribute("user", new UserDTO());
 
@@ -202,6 +227,7 @@ public class SomethingElseController {
 
     @PostMapping("/deleteuser")
     public String deleteUser (@ModelAttribute("user") UserDTO user, Model model) {
+        logger.debug("Logiken för att ta bort en användare.");
 
 
         //boolean user0 = userRepository.findByUsername(user.getUsername()).isPresent();
@@ -213,17 +239,17 @@ public class SomethingElseController {
             MyUser user1 = userService.getUserByUsername(HtmlUtils.htmlEscape(user.getUsername())).get();
 
             if(user1.getRole() != "ADMIN") {
+                logger.debug("Användare har ej role ADMIN.");
 
                 System.out.println("user.getRole: " + user1.getRole());
 
                 try {
                     userService.deleteUser(user1.getId());
                 } catch (UsernameNotFoundException e) {
+                    logger.warn("Användaren " + Maskning.maskEmail(user.getUsername()) + " hittades inte vid borttagning.");
+                    logger.error("UserNameNotFoundException: ", e);
                     // insert logger.warn
                     // logga stack trace the exception e
-                    e.getStackTrace();
-                    e.getMessage();
-                    e.printStackTrace();
                     System.out.println("username not found");
 
                     model.addAttribute("id", user.getUsername());
@@ -235,20 +261,20 @@ public class SomethingElseController {
                 return "userDeleted";
 
             } else {
-
+                logger.warn("ADMIN kan ej bli borttagen.");
                 return "adminCantBeDeleted";
             }
 
         } else {
-
+            logger.warn("Användaren " + Maskning.maskEmail(user.getUsername()) + " hittades inte vid borttagning.");
             model.addAttribute("id", user.getUsername());
             return "userNotFound";
         }
-
     }
 
     @GetMapping("/updateuser")
     public String updatePasswordForm (Model model) {
+        logger.debug("Uppdatering av användarens lösenord-sida.");
 
         model.addAttribute("user", new UpdateUserDTO());
         return "updateUser";
@@ -256,33 +282,40 @@ public class SomethingElseController {
 
     @PostMapping("/updateuser")
     public String updatePassword (@Valid @ModelAttribute("user") UpdateUserDTO user, BindingResult result, Model model, MyUser myUser) {
+        logger.debug("Uppdatering av en användares lösenord har försökts.");
 
         if (result.hasErrors()) {
+            logger.debug("Lösenordets validering misslyckades.");
             return "updateUser";
         }
 
         boolean userExists = userService.getUserByUsername(user.getUsername()).isPresent();
 
         if (userExists) {
+            logger.debug("Användare har hittats.");
             MyUser user1 = userService.getUserByUsername(user.getUsername()).get();
 
             try {
                 userService.updateUser(user1.getId(), user);
             } catch (UsernameNotFoundException e) {
+                logger.warn("Användare " + Maskning.maskEmail(user.getUsername()) + "hittades inte vid uppdatering.");
+                logger.error("UserNameNotFoundException: ", e);
+                logger.error("Kollar vad e.getStackTrace är för något", e.getStackTrace());
                 //logger.warn
                 //logga stack trace exception e
-                e.getStackTrace();
-                e.printStackTrace();
+                //Ta upp på handledning
+//                e.getStackTrace();
+//                e.printStackTrace();
+
                 System.out.println("user with username not found");
             }
-
 
             model.addAttribute("user", user1);
 
             return "updateSuccess";
 
         } else {
-
+            logger.warn("Användare " + Maskning.maskEmail(user.getUsername()) + "hittades inte vid uppdatering.");
             model.addAttribute("id", user.getUsername());
             return "userNotFound";
         }
@@ -294,17 +327,20 @@ public class SomethingElseController {
 //
     @GetMapping("/logout")
     public String logout () {
+        logger.debug("Utloggnings-sida");
         return "logout";
     }
 
     @PostMapping("/logout")
     public String performLogout () {
+        logger.debug("Har loggats ut.");
         System.out.println("logged out");
         return "logoutSuccess";
     }
 
     @GetMapping("/logoutsuccess")
     public String getLogoutSuccess () {
+        logger.debug("Meddelar lyckad utloggnings-sida.");
         return "logoutSuccess";
     }
 //
